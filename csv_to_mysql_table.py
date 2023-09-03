06dc13aaccf1637979d01c3cef6758c6a2bc8d48
animeshjain7 <108515181+animeshjain7@users.noreply.github.com>
2023-09-03 20:26:58 +0530

    Improved for database selection

--- csv_to_mysql_table.py
+++ csv_to_mysql_table.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import pandas as pd
 import time
+
+
 host = "localhost"
 user = 'root'
-password = '#YOUR PASSWORD'                               #put mysql password
-database = '#YOUR DATABASE'                                # put mysql databse name
+password = '#PASSWORD'                                                              
 port = 3306
 
 cursor = None
@@ -14,13 +15,37 @@ try:
         host=host,
         user=user,
         password=password,
-        database=database,
         port=port
     )
+   
+
     if connection.is_connected():
-        print('Connectd to mysql')
         cursor = connection.cursor()
-        file = pd.read_csv('Housing.csv')                   # read csv
+
+        print('\tTYPE 0 TO USE OLD DATABASE ')
+        print('\tTYPE 1 TO CREATE NEW DATABASE ')
+        choice = int(input('\tCHOICE FOR DATABASE : '))
+
+        if choice == 0:
+            show_db_query = "SHOW DATABASES;"
+            cursor.execute(show_db_query)
+            databases = cursor.fetchall()
+            for x in databases:
+                print('\t' + x[0])
+            db_name = input('\tTYPE THE DATABASE YOU WANT TO USE : ')
+            use_db_query = "USE "+ db_name + "; "
+            cursor.execute(use_db_query)
+        if choice == 1:
+            new_db_name = input('\tENTER DATABASE NAME : ')
+            create_new_db_query = "CREATE DATABASE IF NOT EXISTS " + new_db_name + "; "
+            use_db_query = "USE "+new_db_name + "; "
+            cursor.execute(create_new_db_query)
+            cursor.execute(use_db_query)
+
+        connection.commit()
+        print('\tConnected to MySQL')
+        file_path = input('\tEnter the path of CSV file : ')        #CTRL+SHIFT+V for paste in powershell
+        file = pd.read_csv(file_path)                  
 
       
         l = []
@@ -30,6 +55,7 @@ try:
             my_dict = dict(zip(file.columns, l))
           
         create_table_query = ""
+        new_table_name = input('\tENTER NEW TABLE NAME : ')
         sql_dtype = ''
       
         for keys, values in my_dict.items():
@@ -37,37 +63,38 @@ try:
                 sql_dtype = 'INT'
             if my_dict[keys] == str:
                 sql_dtype = 'VARCHAR (255)'
-            create_table_query += f"{keys} {sql_dtype},\n"
+            create_table_query += f"{keys} {sql_dtype},\n\t"
 
-        create_table_query = "CREATE TABLE #table_name(" + create_table_query                # put your table name here at #table_name
-        create_table_query = create_table_query[:-2] + ");"
-        print(create_table_query)
+        create_table_query = "CREATE TABLE "+ new_table_name +" (" + create_table_query                
+        create_table_query = create_table_query[:-3] + ");"
+        print('\t'+create_table_query)
         cursor.execute(create_table_query)
         connection.commit()
-        print("Creating Table in MySql")
+        print("\tCreating Table in MySql")
 
 
       
         count = 0
         for item in file.values:
             count += 1
-            insert_query = f'INSERT INTO #table_name VALUES('                            #  put your table name here at #table_name
+            insert_query = f'INSERT INTO {new_table_name} VALUES('                            
             for a in item:
                 if type(a) == int:
                     insert_query += f"{a},"
                 else:
                     insert_query += f"'{a}',"
             insert_query = insert_query[:-1] + ');'
-            print(f'inserting {count} data')
+            print('\t'+f'inserting {count} data')
             cursor.execute(insert_query)
             connection.commit()
-            print(insert_query)
-
+            print('\t'+insert_query)
+            time.sleep(0.2)
+             
 except mysql.connector.Error as e:
-    print('MYsql error:', e)
+    print('\tMYsql error:', e)
 finally:
     if cursor is not None:
         cursor.close()
     if connection is not None and connection.is_connected():
         connection.close()
-        print('Connection Closed')
+        print('\tConnection Closed')
